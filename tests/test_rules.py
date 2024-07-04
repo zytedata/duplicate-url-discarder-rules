485cf7a4ed733b186cdf58287fa3ae3976fb7cb3
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2024-07-04 16:21:32 +1000

    add tests

---
+++ tests/test_rules.py
@@ -0,0 +1,34 @@
+import json
+
+from url_matcher import Patterns
+
+from duplicate_url_discarder_rules import RULE_PATHS
+
+
+def test_rule_validity():
+    for path in RULE_PATHS:
+        try:
+            with open(path, "r") as f:
+                data = json.loads(f.read())
+        except Exception as e:
+            raise ValueError(f"Something went wrong when reading '{path}': {str(e)}.")
+
+        assert isinstance(
+            data, list
+        ), f"The rules should be a list: '{path}'. Got {type(data)}."
+
+        for rule in data:
+            order = rule.get("order")
+            assert order is not None, path
+            assert isinstance(order, int), path
+
+            url_pattern = rule.get("urlPattern")
+            assert isinstance(url_pattern, dict), path
+            assert isinstance(url_pattern["include"], list), path
+            assert Patterns(url_pattern)
+
+            assert rule.get("processor"), path
+            assert isinstance(rule.get("processor"), str), path
+
+            if "args" in rule:
+                assert isinstance(rule["args"], (list, tuple))
