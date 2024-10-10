70c45f6bf522e080210441dd7a3721404f4b5063
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2024-10-10 16:40:46 +1100

    bump version 0.3.0 → 2024.10.10

--- duplicate_url_discarder_rules/__init__.py
+++ duplicate_url_discarder_rules/__init__.py
@@ -6,4 +6,4 @@ _current_path = Path(__file__).parent.resolve()
 RULE_PATHS: Optional[List[str]] = glob(str(_current_path / "**/*.json"), recursive=True)
 
 
-__version__ = "0.3.0"
+__version__ = "2024.10.10"
